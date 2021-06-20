b31489b4a6f0813bccbbf3512946414321f08c88
redan684 <redan684@gmail.com>
2021-06-20 22:08:50 +0530

    add backend authentication

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
+    'apps.accounts'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +54,20 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'server.urls'
+#configure DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
+# configure Djoser
+DJOSER = {
+    "USER_ID_FIELD": "username"
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
